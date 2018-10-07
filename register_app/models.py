0eb099b3e25958f9723609fadf7ca1c6b86ca9eb
Milad Hatami <miladhzz@gmail.com>
2018-10-08 00:08:41 +0330

    fix models

--- register_app/models.py
+++ register_app/models.py
@@ -8,7 +8,7 @@ class Permission(models.Model):
 
 
 class Users(models.Model):
-    permission = models.ForeignKey(Permission, on_delete=models.SET_NULL)
+    permission = models.ForeignKey(Permission, on_delete=models.SET_NULL, null=True)
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=20)
     mobile = models.CharField(max_length=11)
@@ -20,11 +20,11 @@ class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     publish_date = models.DateTimeField()
-    author = models.ForeignKey(Users, on_delete=models.CASCADE)
+    author = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.SET_NULL)
-    post = models.ForeignKey(Posts, on_delete=models.SET_NULL)
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Posts, on_delete=models.SET_NULL, null=True)
     content = models.TextField()
     comment_date = models.DateTimeField
