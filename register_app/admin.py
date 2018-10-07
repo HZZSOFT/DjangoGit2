0f666dff900e9db24da373a6ea52cb815322b34c
Milad Hatami <miladhzz@gmail.com>
2018-10-08 00:46:46 +0330

    add str method to models and register all models to admin.py

--- register_app/admin.py
+++ register_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import User
 from .models import Permission
+from .models import Post
+from .models import Comment
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Permission)
+admin.site.register(Post)
+admin.site.register(Comment)
--- register_app/models.py
+++ register_app/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Permission(models.Model):
-    user_type = models.BooleanField()
     description = models.CharField(max_length=500)
+    is_Admin = models.BooleanField()
+
+    def __str__(self):
+        return self.description
 
 
 class User(models.Model):
@@ -15,16 +18,25 @@ class User(models.Model):
     address = models.CharField(max_length=500)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.username
 
-class Posts(models.Model):
+
+class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     publish_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    post = models.ForeignKey(Posts, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
     content = models.TextField()
     comment_date = models.DateTimeField
+
+    def __str__(self):
+        return self.content
