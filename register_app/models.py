3b38ce2163ba5da9a3b2ca24d2d63bc8becee40c
Milad Hatami <miladhzz@gmail.com>
2018-10-08 00:26:06 +0330

    change model and admin.py

--- register_app/models.py
+++ register_app/models.py
@@ -7,7 +7,7 @@ class Permission(models.Model):
     description = models.CharField(max_length=500)
 
 
-class Users(models.Model):
+class User(models.Model):
     permission = models.ForeignKey(Permission, on_delete=models.SET_NULL, null=True)
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=20)
@@ -20,11 +20,11 @@ class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     publish_date = models.DateTimeField()
-    author = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     post = models.ForeignKey(Posts, on_delete=models.SET_NULL, null=True)
     content = models.TextField()
     comment_date = models.DateTimeField
