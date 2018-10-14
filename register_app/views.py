29b3eb24accca9d908ade604957b04eaeb504d4b
Milad Hatami <miladhzz@gmail.com>
2018-10-14 23:18:40 +0330

    pass list of object to static html  file and show object info with for loop

--- register_app/views.py
+++ register_app/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    name = 'phone number'
-    value = '09384677005'
-    context ={'title':name,
-              'value':value}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'customers':customers})
+
+class Customer:
+    def __init__(self, name, family, mobile, avg):
+        self.name = name
+        self.family = family
+        self.mobile = mobile
+        self.avg = avg
+
+customers = [
+    Customer('milad', 'hatami', '09384677005',16.25),
+    Customer('ali', 'rezaei', '09169555555', 19.50),
+    Customer('hadi', 'razi', '09121231122', 15)
+]
