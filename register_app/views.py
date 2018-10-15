c03f7561fda5ba2e7fc791768a0337fb1ff8787c
Milad Hatami <miladhzz@gmail.com>
2018-10-16 00:27:24 +0330

    check login user in html static file

--- register_app/views.py
+++ register_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import Post
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'customers':customers})
+    return render(request, 'index.html', {'customers': customers})
+
 
 def posts(request):
-    all = Post.objects.all()
-    return render(request, 'posts.html', {'posts':all})
+    all_post = Post.objects.all()
+    return render(request, 'posts.html', {'posts': all_post})
+
 
 class Customer:
     def __init__(self, name, family, mobile, avg):
@@ -16,8 +19,9 @@ class Customer:
         self.mobile = mobile
         self.avg = avg
 
+
 customers = [
-    Customer('milad', 'hatami', '09384677005',16.25),
+    Customer('milad', 'hatami', '09384677005', 16.25),
     Customer('ali', 'rezaei', '09169555555', 19.50),
     Customer('hadi', 'razi', '09121231122', 15)
 ]
