86054d293354abc3059f612f3f734c3b6d94ec05
Milad Hatami <miladhzz@gmail.com>
2018-10-17 00:11:13 +0330

    add 'post' with form inherited by ModelForm

--- register_app/urls.py
+++ register_app/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^posts', views.posts)
+    url(r'^$', views.index, name='index'),
+    url(r'^posts/new', views.new_post, name='new_post'),
+    url(r'^posts/', views.posts, name='posts')
 ]
 #    path('index/', views.index),
 #    path('posts/', views.posts),
--- register_app/views.py
+++ register_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -10,6 +12,18 @@ def index(request):
 def posts(request):
     all_post = Post.objects.all()
     return render(request, 'posts.html', {'posts': all_post})
+
+
+def new_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('views.posts')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
 
 
 class Customer:
