d674d20fa08ef9486b30069f5bc55cfbfdd1d078
Milad Hatami <miladhzz@gmail.com>
2018-10-27 00:00:27 +0330

    add comment form for Post_detail. handle active comment

--- register_app/views.py
+++ register_app/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
+from .models import Post
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth import logout
@@ -13,8 +13,21 @@ def index(request):
 
 
 def post_detail(request, post):
-    post1 = get_object_or_404(Post, title=post)
-    return render(request, 'post_detail.html', {'post': post1})
+    post = get_object_or_404(Post, title=post)
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect(post)
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'post_detail.html', {'post': post,
+                                                'comments': comments,
+                                                'comment_form': comment_form})
 
 
 def logout_view(request):
